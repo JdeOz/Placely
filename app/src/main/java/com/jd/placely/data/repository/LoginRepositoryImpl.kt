84ffedecdb82a6b174ed4b18ef75c32b0751886b
jdotaz1 <juan.obando.zuniga@ucsp.edu.pe>
2023-06-21 15:03:55 -0500

    Cambio login de Firebase a Supabase.

--- app/src/main/java/com/jd/placely/data/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/jd/placely/data/repository/LoginRepositoryImpl.kt
@@ -1,38 +1,29 @@
 package com.jd.placely.data.repository
 
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.jd.placely.domain.repository.LoginRepository
 import com.jd.placely.util.Constants.SUPABASE_KEY
 import com.jd.placely.util.Constants.SUPABASE_URL
 import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.gotrue.gotrue
 import io.github.jan.supabase.gotrue.providers.builtin.Email
-import kotlinx.coroutines.tasks.await
+import io.github.jan.supabase.postgrest.Postgrest
 
 class LoginRepositoryImpl() : LoginRepository {
-    private val auth = Firebase.auth
+    val client = createSupabaseClient(supabaseUrl = SUPABASE_URL, supabaseKey = SUPABASE_KEY){
+        install(GoTrue)
+        install(Postgrest)
+    }
 
     override suspend fun login(email: String, password: String): Boolean {
         return try {
-            auth.signInWithEmailAndPassword(email, password).await()
+            client.gotrue.loginWith(Email) {
+                this.email = email
+                this.password = password
+            }
             true // Login successful
         } catch (e: Exception) {
             false // Login failed
         }
     }
 }
-
-//class LoginRepositoryImpl() : LoginRepository {
-//    val client = createSupabaseClient(supabaseUrl = SUPABASE_URL, supabaseKey = SUPABASE_KEY){}
-//    val gotrue = client.gotrue
-//
-//    override suspend fun login(email: String, password: String): Boolean {
-//        client.gotrue.loginWith(Email) {
-//            this.email = email
-//            this.password = password
-//        }
-//        return true // Login successful
-//
-//    }
-//}
