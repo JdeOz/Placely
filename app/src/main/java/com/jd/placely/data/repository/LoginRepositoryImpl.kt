ee3c1cb31078c8f7a15330db56ad2a60cc4b0430
jdotaz1 <juan.obando.zuniga@ucsp.edu.pe>
2023-06-21 09:14:45 -0500

    Post con la Rest Api y retrofi2.

--- app/src/main/java/com/jd/placely/data/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/jd/placely/data/repository/LoginRepositoryImpl.kt
@@ -3,6 +3,11 @@ package com.jd.placely.data.repository
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.jd.placely.domain.repository.LoginRepository
+import com.jd.placely.util.Constants.SUPABASE_KEY
+import com.jd.placely.util.Constants.SUPABASE_URL
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.gotrue
+import io.github.jan.supabase.gotrue.providers.builtin.Email
 import kotlinx.coroutines.tasks.await
 
 class LoginRepositoryImpl() : LoginRepository {
@@ -17,3 +22,17 @@ class LoginRepositoryImpl() : LoginRepository {
         }
     }
 }
+
+//class LoginRepositoryImpl() : LoginRepository {
+//    val client = createSupabaseClient(supabaseUrl = SUPABASE_URL, supabaseKey = SUPABASE_KEY){}
+//    val gotrue = client.gotrue
+//
+//    override suspend fun login(email: String, password: String): Boolean {
+//        client.gotrue.loginWith(Email) {
+//            this.email = email
+//            this.password = password
+//        }
+//        return true // Login successful
+//
+//    }
+//}
--- app/src/main/java/com/jd/placely/util/Constants.kt
+++ app/src/main/java/com/jd/placely/util/Constants.kt
@@ -2,4 +2,7 @@ package com.jd.placely.util
 
 object Constants {
     const val SPLASH_SCREEN_DURATION = 2000L
+    const val SUPABASE_KEY =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxemtmZ2Z3Z3picHd0anl5b3hiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODczMDAzNjAsImV4cCI6MjAwMjg3NjM2MH0._9ibrAtn8iO3f4LEesguyAX8bOLHxErh04eRB8PszhY"
+    const val SUPABASE_URL = "https://kqzkfgfwgzbpwtjyyoxb.supabase.co"
 }
